7a7b186dfef9f415d0913843ef763e1bcdaa9d96
nhunhu-93 <tunhu388@gmail.com>
2024-10-20 17:19:40 +0700

    ...

--- partials/header.php
+++ partials/header.php
@@ -7,8 +7,10 @@
     <title>Quản lý KTX</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0-alpha1/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
-    <link rel="stylesheet" href="../assets/css/main.css">
-    <link rel="stylesheet" href="../assets/css/responsive.css">
+    <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/utilities/font-size/font-size.css">
+    <link rel="stylesheet" href="https://unpkg.com/bs-brain@2.0.4/utilities/padding/padding.css">
+    <link rel="stylesheet" href="../public/assets/css/main.css">
+    <link rel="stylesheet" href="../public/assets/css/responsive.css">
     <!-- Option 1: Include in HTML -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 </head>
--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -1,25 +1,4 @@
 <?php
-// Khởi tạo phiên làm việc
-if (!isset($_SESSION)) {
-    session_start();
-}
-// Kiểm tra xem vai trò đã được lưu trong session hay chưa
-if (isset($_SESSION['Role'])) {
-    $role = $_SESSION['Role'];
-    if ($role === 'user') {
-        echo "<script>alert('Bạn không có quyền truy cập vào trang này.')
-        window.location.href='../../user/dashboard.php';
-        </script>";
-        die();
-    }
-}
-// nếu chưa đăng nhập thì chuyển hướng về trang đăng nhập
-else {
-    echo "<script>alert('Vui lòng đăng nhập.')
-    window.location.href='./index.php';
-    </script>";
-    die();
-}
 
 // Bắt đầu nội dung trang
 ob_start();
