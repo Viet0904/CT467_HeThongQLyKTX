704e1a0c26e931771712f02b22e98c140c2827a2
nhunhu-93 <tunhu388@gmail.com>
2024-11-03 20:57:34 +0700

    cập nhật hiện dữ liệu các trang thông tin

--- public/user/dashboard.php
+++ public/user/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 include_once __DIR__ . '/../../partials/header.php';
-include_once __DIR__ . '/../../partials/heading.php';
+include_once __DIR__ . '/../../partials/navbar.php';
 ?>
 
 <body>
