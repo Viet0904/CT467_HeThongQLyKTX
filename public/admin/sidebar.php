9a8a96ff98f92ff7caa8004144e9037a013f2964
nhunhu-93 <tunhu388@gmail.com>
2024-11-12 21:15:00 +0700

    Update sidebar and dashboard to enhance UI elements and display total revenue

--- public/admin/sidebar.php
+++ public/admin/sidebar.php
@@ -8,11 +8,11 @@
         <li class="section-header">Main</li>
         <li class="p-0 ms-1"><a href="./dashboard.php" class="py-2 ps-3"><i class="fas fa-tachometer-alt"></i> Trang điều khiển</a></li>
         <li class="p-0 ms-1"><a href="./student_list.php" class="py-2 ps-3"><i class="fas fa-users"></i> Danh sách sinh viên</a></li>
-        <li class="p-0 ms-1"><a href="./dangkyphong_sv.php" class="py-2 ps-3"><i class="fas fa-users"></i> Danh sách thuê phòng</a></li>
+        <li class="p-0 ms-1"><a href="./dangkyphong_sv.php" class="py-2 ps-3"><i class="fas fa-key"></i> Danh sách thuê phòng</a></li>
         <!-- Master List Section -->
         <li class="section-header">Danh sách chính</li>
         <li class="p-0 ms-1"><a href="./room_list.php" class="py-2 ps-3"><i class="fas fa-door-open"></i> Danh sách phòng</a></li>
-        <li class="p-0 ms-1"><a href="./ql_dangky_ktx.php" class="py-2 ps-3"><i class="fas fa-user-check"></i> Quản lý đăng ký KTX</a></li>
+        <li class="p-0 ms-1"><a href="./manage_diennuoc.php" class="py-2 ps-3"><i class="fas fa-lightbulb"></i> Quản lý điện nước</a></li>
         <li class="p-0 ms-1"><a href="./ql_thuephong.php" class="py-2 ps-3"><i class="fas fa-bed"></i> Quản lý thuê phòng</a></li>
         
         <!-- Reports Section -->
