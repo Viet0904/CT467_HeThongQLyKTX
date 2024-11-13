6d9296099278f2659a3c3c41b79f63418a7dd8c8
PhatB2111894 <phatb2111894@student.ctu.edu.vn>
2024-11-13 14:48:50 +0700

    Update anhnenlogin

--- public/index.php
+++ public/index.php
@@ -94,7 +94,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <style>
         body {
-            background: url('./assets/images/cover.png') no-repeat center center fixed;
+            background: url('./assets/images/anhktx.jpg') no-repeat center center fixed;
             background-size: cover;
             height: 100vh;
         }
