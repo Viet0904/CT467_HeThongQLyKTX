4ee708d8e0b810476f2052f662055c060cadfdc6
PhatB2111894 <phatb2111894@student.ctu.edu.vn>
2024-11-13 20:42:09 +0700

    Update maytrangchoadmin

--- public/admin/sidebar.php
+++ public/admin/sidebar.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <div class="sidebar px-0">
     <div class="sidebar-header">
         <i class="fas fa-tachometer-alt"></i>
@@ -17,6 +21,13 @@
         <li class="p-0 ms-1"><a href="./room_list.php" class="py-2 ps-3"><i class="fas fa-door-open"></i> Danh sách phòng</a></li>
         <li class="p-0 ms-1"><a href="./manage_diennuoc.php" class="py-2 ps-3"><i class="fas fa-lightbulb me-3"></i> Quản lý điện nước</a></li>
         <li class="p-0 ms-1"><a href="./ql_thuephong.php" class="py-2 ps-3"><i class="fas fa-bed"></i> Quản lý thuê phòng</a></li>
+        <!-- Additional Options for Admin -->
+        <?php if ($_SESSION['Role'] === 'admin'): ?>
+            <li class="p-0 ms-1"><a href="./manage_employees.php" class="py-2 ps-3"><i class="fas fa-user-shield"></i> Quản lý nhân viên</a></li>
+            <li class="p-0 ms-1"><a href="./manage_khu.php" class="py-2 ps-3"><i class="fas fa-map-marker-alt"></i> Quản lý khu</a></li>
+            <li class="p-0 ms-1"><a href="./manage_day.php" class="py-2 ps-3"><i class="fas fa-columns"></i> Quản lý dãy</a></li>
+            <li class="p-0 ms-1"><a href="./manage_class.php" class="py-2 ps-3"><i class="fas fa-graduation-cap"></i> Quản lý lớp</a></li>
+        <?php endif; ?>
 
         <!-- Reports Section -->
         <li class="section-header">Báo cáo</li>
